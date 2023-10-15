98a8ad07e3cf6df7c1122c65b74ba516c050fb9a
AbbosIsmailov <abbosismailov@gmail.com>
2023-10-15 23:45:06 +0500

    continue

---
+++ handlers/users/start.py
@@ -0,0 +1,30 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+from insta import instadownloader_func
+from aiogram.dispatcher.filters import Text
+from loader import dp
+
+
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    await message.answer(f"Salom, {message.from_user.full_name}!\n Menga instagramdan link tashlang")
+
+@dp.message_handler(Text(startswith="https://www.instagram.com/"))
+async def salom(msg: types.Message):
+    link = msg.text
+    data = instadownloader_func(link)
+    if data == "Bad":
+        await msg.answer("Bu URL manzilini aniqlay olmadik")
+    else:
+        if data['type'] == "image":
+            await msg.answer_photo(photo=data['media'])
+
+        elif data['type']  == "video":
+            await msg.answer_video(video=data['media'])
+        
+        elif data['type']  == "carousel":
+            for i in data['media']:
+                await msg.answer_document(document=i['media'])
+        
+        else:
+            await msg.answer("Bu URL manzilini aniqlay olmadik")
---
+++ insta.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def instadownloader_func(link):
+    import requests
+
+    url = "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index"
+
+    querystring = {"url": link}
+
+    headers = {
+        "X-RapidAPI-Key": "166dbbdddemsh19c386e1d2d8066p100816jsnc5126b1f27a5",
+        "X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    rest = json.loads(response.text)
+
+    dict = {}
+    if 'error' in rest:
+        return "Bad error"
+    else:
+        if rest['Type'] == 'Post-Image':
+            dict['type'] = 'image'
+            dict['media'] = rest['media']
+            return dict
+        elif rest['Type'] == 'Post-Video':
+            dict['type'] = 'video'
+            dict['media'] = rest['media']
+            return dict
+        elif rest['Type'] == 'Carousel':
+            dict['type'] = 'carousel'
+            dict['media'] = rest['media']
+            return dict
+        else:
+            return "Bad"
